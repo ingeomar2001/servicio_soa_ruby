f1ecaa5ed1a1634fdb2d150e3e051f249b16d2de
Omar Gutierrez <omargutierrez@uninorte.edu.co>
2017-04-16 00:37:16 +0000

    modificado

--- app/controllers/servicio_soa_controller.rb
+++ app/controllers/servicio_soa_controller.rb
@@ -9,7 +9,7 @@ class ServicioSoaController < ApplicationController
 
   def itunes
     if params[:term].nil?
-      @term = "ninaemilia"
+      @term = "shakira"
     else
       @term = params[:term]
     end
